a7fbaae1bbcc5196b3ea7fb1718922d5fa5d74bf
psychose <fetrasarobidy3@gmail.com>
2024-02-26 16:11:42 +0100

    creation de la fenetre

--- src/window2/window2.java
+++ src/window2/window2.java
@@ -3,10 +3,20 @@ package window2;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JSpinner;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
 
 public class window2 {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -36,8 +46,71 @@ public class window2 {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 581, 374);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(139, 52, 129, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(197, 83, 71, 20);
+		panel.add(spinner);
+		
+		JSpinner spinner_1 = new JSpinner();
+		spinner_1.setBounds(197, 114, 71, 20);
+		panel.add(spinner_1);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(29, 55, 46, 14);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(29, 86, 46, 14);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setBounds(29, 117, 46, 14);
+		panel.add(lblNewLabel_2);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(302, 52, 253, 194);
+		panel.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(29, 155, 89, 23);
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(179, 155, 89, 23);
+		panel.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(29, 205, 89, 23);
+		panel.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		btnNewButton_3.setBounds(179, 205, 89, 23);
+		panel.add(btnNewButton_3);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		lblNewLabel_3.setBounds(29, 310, 46, 14);
+		panel.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("New label");
+		lblNewLabel_4.setBounds(129, 310, 46, 14);
+		panel.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("New label");
+		lblNewLabel_5.setBounds(509, 310, 46, 14);
+		panel.add(lblNewLabel_5);
 	}
-
 }
